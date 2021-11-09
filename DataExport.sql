696eee9b03322fa52621362ba9d4cfc245b6e880
kevinchang <kevinjchang98@gmail.com>
2021-11-09 10:28:30 -0800

    remove root access for creating stored procedure

--- DataExport.sql
+++ DataExport.sql
@@ -377,7 +377,7 @@ UNLOCK TABLES;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `listFlyableAircraft`(idCustomerIn INT)
+CREATE PROCEDURE `listFlyableAircraft`(idCustomerIn INT)
 BEGIN
 SELECT
     idAircraft,

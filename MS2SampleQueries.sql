89e85049bd7006107d75ec89ac23995871e068c6
kevinchang <kevinjchang98@gmail.com>
2021-11-12 19:40:24 -0800

    update stored procedure

--- MS2SampleQueries.sql
+++ MS2SampleQueries.sql
@@ -17,3 +17,27 @@ WHERE
     )
 ORDER BY
     class;
+
+SELECT
+    a.idCustomer,
+    nameFirst,
+    nameLast,
+    idRating
+FROM
+    CustomerHasRating
+    RIGHT JOIN (
+        SELECT
+            Reservation.idCustomer,
+            Customer.nameFirst,
+            Customer.nameLast
+        FROM
+            Reservation
+            LEFT JOIN Aircraft ON Reservation.idAircraft = Aircraft.idAircraft
+            LEFT JOIN Customer ON Reservation.idCustomer = Customer.idCustomer
+        WHERE
+            isHighPerformance = 1
+        GROUP BY
+            Reservation.idCustomer
+    ) a ON a.idCustomer = CustomerHasRating.idCustomer
+ORDER BY
+    idCustomer;
--- StoredProcedure.sql
+++ StoredProcedure.sql
@@ -2,55 +2,66 @@ DELIMITER //
 CREATE PROCEDURE `listFlyableAircraft` (idCustomerIn INT)
 BEGIN
 SELECT
-    idAircraft,
-    class,
-    tachTime,
-    rentalRate,
-    annualInspectionDate,
-    isTailwheel,
-    isComplex,
-    isHighPerformance
+    Flyable.*,
+    max(datePerformed) AS mostRecentMaint
 FROM
-    CustomerHasRating
-    INNER JOIN Aircraft ON class = idRating
-    AND (
-        Aircraft.isTailwheel = EXISTS(
-            SELECT
-                *
-            FROM
-                CustomerHasRating
-            WHERE
-                idCustomer = idCustomerIn
-                AND idRating = 'TW'
-        )
-        OR Aircraft.isTailWheel = 0
-    )
-    AND (
-        Aircraft.isComplex = EXISTS(
-            SELECT
-                *
-            FROM
-                CustomerHasRating
-            WHERE
-                idCustomer = idCustomerIn
-                AND idRating = 'CP'
-        )
-        OR Aircraft.isComplex = 0
-    )
-    AND (
-        Aircraft.isHighPerformance = EXISTS(
-            SELECT
-                *
-            FROM
-                CustomerHasRating
-            WHERE
-                idCustomer = idCustomerIn
-                AND idRating = 'HP'
-        )
-        OR Aircraft.isHighPerformance = '0'
-    ) -- Also check annual date and if maint schedule exists for it?
-WHERE
-    idCustomer = idCustomerIn
+    Maintenance
+    INNER JOIN (
+        SELECT
+            idAircraft,
+            class,
+            100HrDueTime - tachTime as timeBefore100Hr,
+            rentalRate,
+            annualInspectionDate,
+            isTailwheel,
+            isComplex,
+            isHighPerformance
+        FROM
+            CustomerHasRating
+            INNER JOIN Aircraft ON class = idRating
+            AND (
+                Aircraft.isTailwheel = EXISTS(
+                    SELECT
+                        *
+                    FROM
+                        CustomerHasRating
+                    WHERE
+                        idCustomer = idCustomerIn
+                        AND idRating = 'TW'
+                )
+                OR Aircraft.isTailWheel = 0
+            )
+            AND (
+                Aircraft.isComplex = EXISTS(
+                    SELECT
+                        *
+                    FROM
+                        CustomerHasRating
+                    WHERE
+                        idCustomer = idCustomerIn
+                        AND idRating = 'CP'
+                )
+                OR Aircraft.isComplex = 0
+            )
+            AND (
+                Aircraft.isHighPerformance = EXISTS(
+                    SELECT
+                        *
+                    FROM
+                        CustomerHasRating
+                    WHERE
+                        idCustomer = idCustomerIn
+                        AND idRating = 'HP'
+                )
+                OR Aircraft.isHighPerformance = '0'
+            )
+        WHERE
+            idCustomer = idCustomerIn
+            AND annualInspectionDate > NOW() + INTERVAL 1 DAY
+            AND 100HrDueTime - tachTime > 0.1
+    ) Flyable ON Maintenance.idAircraft = Flyable.idAircraft
+GROUP BY
+    Flyable.idAircraft
 ORDER BY
     class,
     rentalRate;
